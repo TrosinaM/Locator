8fdc82c937a1c2575e259a9409b1dbd2bd2838a5
TrosinaM <Pass0311>
2016-04-12 20:30:01 +0300

    new 1

--- src/main/java/Locator.java
+++ src/main/java/Locator.java
@@ -15,42 +15,34 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by Админ on 10.04.2016.
  */
-public class Locator extends TestCase{
-    private  static WebDriver driver ;
+public class Locator extends BaseTest {
 
-    @BeforeClass
-    public void setUp() throws Exception {
-        driver = new FirefoxDriver();
-        this.driver.get("https://en.wikipedia.org/wiki/Main_Page");
-
-    }
 
     @Test
     public void test() throws Exception {
         WebElement element = driver.findElement(By.name("search"));//find element by name
         element.sendKeys("Apple");
         element.submit();
-        assertEquals(driver.getTitle(),"Apple - Wikipedia, the free encyclopedia");
+        assertEquals(driver.getTitle(), "Apple - Wikipedia, the free encyclopedia");
 
     }
 
-    @Test public void test1() throws Exception {
+    @Test
+    public void test1() throws Exception {
         WebElement element = driver.findElement(By.xpath("//div[@id='simpleSearch']/input[1]"));//find element by
         element.sendKeys("Banana");
         element.submit();
-        //assertEquals(driver.findElement(By.id("");
+        assertEquals(driver.findElement(By.linkText("flowering plants")).getText(), "/wiki/Flowering_plant");
 
 
     }
-    @Test public void test2() throws Exception {
-        WebElement element = driver.findElement(By.id("searchInput") );//find element by id
+
+    @Test
+    public void test2() throws Exception {
+        WebElement element = driver.findElement(By.id("searchInput"));//find element by id
         element.sendKeys("Pineapple");
         element.submit();
 
     }
 
-       @AfterClass
-    public  void tearDown() throws Exception {
-        this.driver.quit();
-    }
 }
